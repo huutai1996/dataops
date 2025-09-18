8a7046d92160654f0b3abadc4842d8a466a84344
huutai1996 <64980049+huutai1996@users.noreply.github.com>
2025-09-18 08:16:33 +0000

    add AuthorsAges.py

--- spark/AuthorsAges.py
+++ spark/AuthorsAges.py
@@ -1,1 +1,11 @@
-sada
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import avg
+
+# Initialize Spark session
+spark = (SparkSession.builder.appName("AuthorsAges").getOrCreate())
+# Create DataFrame
+data = spark.createDataFrame([("Brooke", 20), ("Denny", 31), ("Jules", 30),("TD", 35), ("Brooke", 25)], ["name", "age"])
+# Group by name and calculate average age
+avg_data = data.groupby("name").agg(avg("age").alias("average_age"))
+# Show results
+avg_data.show()
